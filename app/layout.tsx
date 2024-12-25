a6ac716d1c31804c0c2985c8fc82f7f3732982e5
Steven Lynn <i@stv.lol>
2024-12-25 21:52:05 +0800

    add OG info

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,32 @@ const notoSerifJP = Noto_Serif_JP({
 export const metadata: Metadata = {
   title: 'Twitter Yearly Kanji',
   description: 'Generate your yearly kanji based on your tweets',
+  icons: {
+    icon: '/favicon.png',
+  },
+  openGraph: {
+    title: 'Twitter Yearly Kanji',
+    description: 'Generate your yearly kanji based on your tweets',
+    type: 'website',
+    url: 'https://yourwebsite.com', 
+    images: [
+      {
+        url: '/favicon.png',
+        width: 512,
+        height: 512,
+        alt: 'Twitter Yearly Kanji',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Twitter Yearly Kanji',
+    description: 'Generate your yearly kanji based on your tweets',
+    images: ['/favicon.png'],
+    creator: '@twiam_jp',
+  },
+  viewport: 'width=device-width, initial-scale=1',
+  themeColor: '#ffffff',
 };
 
 export default function RootLayout({
