b5fc61bfc8e68fe8fed3ed58c0939b68954b365d
Steven Lynn <i@stv.lol>
2024-12-25 23:12:22 +0800

    update ttl to 180s

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
   BASE_URL: process.env.NEXT_PUBLIC_DIFY_BASE_URL?.replace('http:', 'https:'),
   API_KEY: process.env.NEXT_PUBLIC_DIFY_API_KEY,
-  TIMEOUT: 80000, // 80 seconds
+  TIMEOUT: 180000, // 180 seconds
 } as const;
